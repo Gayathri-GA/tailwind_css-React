9900b697c58a42101aa5a1d3ae0f9d5dc050f552
gayathrid <gayathrid@geekyants.com>
2021-07-28 02:12:28 +0530

    Responsive design using tailwind css added

---
+++ responsive_design/src/App.js
@@ -0,0 +1,28 @@
+// Responsive Design
+// Using responsive utility variants to build adaptive user interfaces.
+
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
