f6b49ea508aaee7a6873db982ba95538f77a4886
gayathrid <gayathrid@geekyants.com>
2021-08-03 10:58:49 +0530

    Added landing page

---
+++ landing_page/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      {/* header */}
+      <div>
+        <img src="https://freepikpsd.com/media/2020/11/academy-png-6.png" alt="Logo" />
+      </div>
+      {/* hero */}
+      <div>
+
+      </div>
+      {/* features */}
+      <div>
+
+      </div>
+      {/* footer and newsletter */}
+      <div>
+
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
